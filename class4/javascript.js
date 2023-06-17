d7547f0541deddbacc71e349b33d5bc93d854503
Fatimaarshad10 <fatimaarshad118@gmail.com>
2023-06-17 10:01:21 +0500

    class4

---
+++ class4/javascript.js
@@ -0,0 +1,128 @@
+// Variables ​
+
+// ----------Question no 1------------
+// var message = "Hello, World!";
+// console.log(message);
+
+// // ----------Question no 2-----------------
+// let score = 85;
+// console.log(score);
+
+// // ----------Question no 3-----------------
+// let x = 5;
+// let y = 10;
+// let sum = x + y;
+// console.log(sum);
+
+// // ----------Question no 4-----------------
+// const PI = 3.14159;
+// let radius = 5;
+// let circle = 2 * PI * radius;
+// console.log(circle);
+
+
+
+// Arithmetic Operators
+// let x = 10;
+// let y = 5;
+// // addition 
+// let sum = x + y;
+// console.log("Sum:", sum);
+// // subtraction
+// let difference = x - y;
+// console.log("Difference:", difference);
+// // multiplication
+// let product = x * y;
+// console.log("Product:", product);
+// // division
+// let divide = x / y;
+// console.log("Quotient:", divide);
+// // remainder
+// let remainder = x % y;
+// console.log("Remainder:", remainder);
+
+// // Addition assignment (+=)
+// let a = 5;
+// a += 3; 
+// console.log(a); 
+// // Subtraction assignment (-=)
+// let b = 10;
+// b -= 4; 
+// console.log(b); 
+// // Multiplication assignment (*=)
+// let c = 3;
+// c *= 5; 
+// console.log(c); 
+// // Division assignment (/=)
+// let d = 20;
+// d /= 4; 
+// console.log(d); 
+
+// Comparison Operators
+// Equal to (==):
+// let a = 5;
+// let b = 5;
+// console.log(a == b)
+
+// // Not equal to (!=):
+// let c = 10;
+// let d = 7;
+// console.log(c != d); 
+
+// // Greater than (>):
+// let i = 8;
+// let j = 5;
+// console.log(i > j); 
+
+// // Less than (<):
+// let k = 3;
+// let l = 7;
+// console.log(k < l); 
+
+// // Greater than or equal to (>=):
+// let m = 5;
+// let n = 5;
+// console.log(m >= n); 
+
+// Less than or equal to (<=):
+// let o = 4;
+// let p = 9;
+// console.log(o <= p); 
+
+// Logical AND (&&):
+// let a = true;
+// let b = false;
+// console.log(a && b); 
+
+// // Logical OR (||):
+// let c = true;
+// let d = false;
+// console.log(c || d); 
+
+// // Logical NOT (!):
+// let e = true;
+// console.log(!e); 
+
+
+// Conditions 
+
+// let temperature = 25;
+
+// if (temperature > 30) {
+//   console.log("It's a hot day!");
+// } else {
+//   console.log("It's not a hot day.");
+// }
+
+// let hour =  6;
+// let greeting;
+
+// if (hour < 12) {
+//   greeting = "Good morning!";
+// } else if (hour < 18) {
+//   greeting = "Good afternoon!";
+// } else {
+//   greeting = "Good evening!";
+// }
+
+// console.log(greeting);

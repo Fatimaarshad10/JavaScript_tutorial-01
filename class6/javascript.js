2b6798c68b48bb0e6c5ef06a5d46f1dc58ac3617
Fatimaarshad10 <fatimaarshad118@gmail.com>
2023-06-24 18:32:04 +0500

    commit

---
+++ class6/javascript.js
@@ -0,0 +1,110 @@
+// while loop 
+
+// var count = 0;
+
+// while (count < 5) {
+//   document.write("Count:" +count  + "<br />" );
+// count++;
+// console.log(count)
+// }
+
+// // do while loop 
+// var count = 0;
+
+// do {
+//   console.log("Count: " + count);
+//   count++;
+// } while (count < 5);
+
+// // for loop 
+
+// for (var i = 10 ; i > 5; i--) {
+//     if(i === 6){
+//     document.write("Count: " + i);
+
+//     }else{
+//         console.log('no value');
+
+//     }
+//   }
+// for(var i = 0 ; i < 5;i++){
+//     for(var m = 1 ; m < 5 ; m++){
+//         console.log('inner loop' +m)
+        
+//     }
+//     console.log('outer loop' +i)
+//     var add = m+ i
+//     console.log(add)
+    
+// }
+
+// var x = 1;
+// document.write("Entering the loop<br /> ");
+// while (x < 20)
+// {
+//  if (x == 5){ 
+//  break; // breaks out of loop completely
+//  }
+//  x = x + 1;
+//  document.write( x + "<br />");
+// }
+// document.write("Exiting the loop!<br /> ");
+
+// var x = 1;
+// document.write("Entering the loop<br /> ");
+// while (x < 10)
+// {
+//  x = x + 1;
+//  if (x == 5){ 
+//  continue; // skill rest of the loop body
+//  }
+//  document.write( x + "<br />");
+// }
+// document.write("Exiting the loop!<br /> ")
+// -------------document.style-------------------
+// let red = document.getElementById('red')
+// red.style.color= 'red'
+
+// let blue_heading = document.getElementById('blue-heading')
+// blue_heading.style.backgroundColor = 'blue'
+// blue_heading.style.width = '100%'
+// blue_heading.style.height = '100vh'
+// blue_heading.style.fontSize = '30px'
+// blue_heading.style.margin = '20px 20px 20px 20px'
+// blue_heading.style.padding = '20px 20px 20px 20px'
+
+
+// -------------innerHTML and write-------------------
+// let list_one = document.getElementById('list-1')
+// list_one.innerHTML = 'Burger'
+// list_one.innerText = 'pizza'
+
+// var list_two = document.getElementById('list-2');
+// list_two.innerHTML = '<em>noodles</em>';
+
+// var list_three = document.getElementById('list-3');
+// document.write('New content for list 3');
+
+
+// ------------- Functions -------------------
+
+// function myFunction() {
+//     console.log('Hello! This is myFunction');
+//     document.getElementById('idFunction').innerHTML = '<p>This is function block paragraph first</p>';
+    
+// }
+// myFunction()
+
+
+// ---------------Problem no 1----------------
+// function changeBackgroundColor() {
+//     let element = document.getElementById('myElement');
+//     element.style.backgroundColor = 'blue';
+//   }
+
+
+// ------------------Problem no 2-----------
+// var element = document.getElementById("myElement");
+
+// // Change the innerHTML of the element
+// element.innerHTML = "Yes";
